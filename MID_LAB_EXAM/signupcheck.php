0f7831ce4c36441f56d527b17e74b82d83d68f67
rahmansabrina <sabrina.shoshi14@gmail.com>
2021-10-18 16:18:50 +0600

    till done

---
+++ MID_LAB_EXAM/signupcheck.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+		if($_POST['id'] != ""){
+			if($_POST['password'] != ""){
+				if($_POST['name'] != ""){
+
+					$myfile = fopen('user.txt', 'a');
+					$user = $_POST['id']."|".$_POST['password']."|".$_POST['name']."\r\n";
+
+					fwrite($myfile, $user);
+					fclose($myfile);
+
+					header('location: login.html');
+				}else{
+					echo "Invalid name...";
+				}	
+			}else{
+				echo "Invalid password...";
+			}
+		}else{
+			echo "Invalid id...";
+		}
+	}
+?>
